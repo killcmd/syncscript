c16006ba4b081f50afa43f7da98ac4331ac4e64a
Ferdinand Vasquez <killcmd@gmail.com>
2019-04-14 17:25:45 -0400

    Update bloat.sh

--- bloat.sh
+++ bloat.sh
@@ -1,3 +1,11 @@
 repo init -u https://github.com/killcmd/platform_manifest.git -b pie
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+echo "ccache -M 25G" >> build_me.sh
+echo "ccache -c" >> build_me.sh
+echo "repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags" >> build_me.sh
+echo "source build/envsetup.sh" >> build_me.sh
+echo "lunch rr_mata-userdebug" >> build_me.sh
+echo "make clean" >> build_me.sh
+echo "brunch mata" >> build_me.sh
+chmod a+x ./build_me.sh
 rm ./bloat.sh
