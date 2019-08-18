0e5319af02ebd58353ce89bb31b06fc690272f5d
Ferdinand Vasquez <killcmd@gmail.com>
2019-08-18 14:06:20 -0400

    Update bloat.sh

--- bloat.sh
+++ bloat.sh
@@ -1,11 +1,11 @@
 repo init -u git://github.com/killcmd/manifest-1.git -b op7p
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-echo "ccache -M 25G" >> buildin.sh
-echo "ccache -c" >> buildin.sh
-echo "repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags" >> buildin.sh
-echo "source build/envsetup.sh" >> buildin.sh
-echo "lunch aosp_guacamole-userdebug" >> buildin.sh
-echo "make clean" >> buildin.sh
-echo "make aex -j$(nproc --all)" >> buildin.sh
-chmod a+x ./buildin.sh
+echo "ccache -M 25G" >> guac.sh
+echo "ccache -c" >> guac.sh
+echo "repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags" >> guac.sh
+echo "source build/envsetup.sh" >> guac.sh
+echo "lunch aosp_guacamole-userdebug" >> guac.sh
+echo "make clean" >> guac.sh
+echo "make aex -j$(nproc --all)" >> guac.sh
+chmod a+x ./guac.sh
 rm ./bloat.sh
