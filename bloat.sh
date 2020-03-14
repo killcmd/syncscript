4326de9ad04f666931c44500c1e187da0fd88fb4
Ferdinand Vasquez <killcmd@gmail.com>
2020-03-14 17:42:55 -0400

    Update bloat.sh

--- bloat.sh
+++ bloat.sh
@@ -1,11 +1,17 @@
-repo init -u git://github.com/killcmd/manifest-1.git -b op7p
+repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 10.x
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> ./.repo/local_manifest/AEX_manifest.xml
+echo '<manifest>' >> ./.repo/local_manifest/AEX_manifest.xml
+echo '  <project name="killcmd/device_oneplus_guacamole" path="device/oneplus/guacamole" remote="github" revision="AEX" />' >> ./.repo/local_manifest/AEX_manifest.xml
+echo '  <project name="potatodevices/device_oneplus_common" path="device/oneplus/common" remote="github" revision="croquette-release" />' >> ./.repo/local_manifest/AEX_manifest.xml
+echo '  <project name="killcmd/device_oneplus_guacamole" path="device/oneplus/sm8150-common" remote="github" revision="AEX" />' >> ./.repo/local_manifest/AEX_manifest.xml
+echo '  <project name="potatodevices/kernel_oneplus_sm8150" path="kernel/oneplus/sm8150" remote="github" revision="croquette-release" />' >> ./.repo/local_manifest/AEX_manifest.xml
+echo '</manifest>' >> ./.repo/local_manifest/AEX_manifest.xml
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-echo "ccache -M 25G" >> guac.sh
-echo "ccache -c" >> guac.sh
-echo "repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags" >> guac.sh
-echo "source build/envsetup.sh" >> guac.sh
-echo "lunch aosp_guacamole-userdebug" >> guac.sh
-echo "make clean" >> guac.sh
-echo "make aex -j$(nproc --all)" >> guac.sh
-chmod a+x ./guac.sh
+echo "ccache -c" >> Taste.sh
+echo "repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags" >> Taste.sh
+echo "source build/envsetup.sh" >> Taste.sh
+echo "lunch aosp_guacamole-userdebug" >> Taste.sh
+echo "make clean" >> Taste.sh
+echo "make aex -j$(nproc --all)" >> Taste.sh
+chmod a+x  >> Taste.sh
 rm ./bloat.sh
