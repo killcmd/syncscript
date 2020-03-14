0a10df84535ddb26e456ff2a1159e19bd203847b
Ferdinand Vasquez <killcmd@gmail.com>
2020-03-14 18:17:32 -0400

    Update bloat.sh

--- bloat.sh
+++ bloat.sh
@@ -1,12 +1,12 @@
 repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 10.x
-mkdir ./.repo/local_manifest
-echo '<?xml version="1.0" encoding="UTF-8"?>' >> ./.repo/local_manifest/AEX_manifest.xml
-echo '<manifest>' >> ./.repo/local_manifest/AEX_manifest.xml
-echo '  <project name="killcmd/device_oneplus_guacamole" path="device/oneplus/guacamole" remote="github" revision="AEX" />' >> ./.repo/local_manifest/AEX_manifest.xml
-echo '  <project name="potatodevices/device_oneplus_common" path="device/oneplus/common" remote="github" revision="croquette-release" />' >> ./.repo/local_manifest/AEX_manifest.xml
-echo '  <project name="killcmd/device_oneplus_guacamole" path="device/oneplus/sm8150-common" remote="github" revision="AEX" />' >> ./.repo/local_manifest/AEX_manifest.xml
-echo '  <project name="potatodevices/kernel_oneplus_sm8150" path="kernel/oneplus/sm8150" remote="github" revision="croquette-release" />' >> ./.repo/local_manifest/AEX_manifest.xml
-echo '</manifest>' >> ./.repo/local_manifest/AEX_manifest.xml
+mkdir ./.repo/local_manifests
+echo '<?xml version="1.0" encoding="UTF-8"?>' >> ./.repo/local_manifests/AEX_manifest.xml
+echo '<manifest>' >> ./.repo/local_manifests/AEX_manifest.xml
+echo '  <project name="killcmd/device_oneplus_guacamole" path="device/oneplus/guacamole" remote="github" revision="AEX" />' >> ./.repo/local_manifests/AEX_manifest.xml
+echo '  <project name="potatodevices/device_oneplus_common" path="device/oneplus/common" remote="github" revision="croquette-release" />' >> ./.repo/local_manifests/AEX_manifest.xml
+echo '  <project name="killcmd/device_oneplus_guacamole" path="device/oneplus/sm8150-common" remote="github" revision="AEX" />' >> ./.repo/local_manifests/AEX_manifest.xml
+echo '  <project name="potatodevices/kernel_oneplus_sm8150" path="kernel/oneplus/sm8150" remote="github" revision="croquette-release" />' >> ./.repo/local_manifests/AEX_manifest.xml
+echo '</manifest>' >> ./.repo/local_manifests/AEX_manifest.xml
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 echo "ccache -c" >> Taste.sh
 echo "repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags" >> Taste.sh
